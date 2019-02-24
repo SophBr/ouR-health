8fc5526207b6f33b2048f7f73b8e749124af2683
Jan Simson <jan.simson@gmx.de>
2019-02-23 23:18:48 -0500

    Added inputs

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,4 +1,5 @@
 library(shinydashboard)
+source("../../stateList.R")
 
 dashboardPage(
   dashboardHeader(title = "ouRhealth"),
@@ -12,14 +13,23 @@ dashboardPage(
     tabItems(
       # First tab content
       tabItem(tabName = "dashboard",
-              fluidRow(
-                box(plotOutput("plot1", height = 250)),
-                
-                box(
-                  title = "Controls",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50)
-                )
-              )
+        fluidRow(
+          box(
+            width = 12, # full width
+            fluidRow(
+              column(selectInput("state", "State:", append(c("All" = NA), stateList)), width = 12),
+              column(sliderInput("age", label = "Age:", value = NA, min = 18, max = 120), width = 6),
+              column(selectInput("sex", "Sex:", c("male" = 1, "female" = 2)), width = 6)
+            )
+          ),
+          
+          box(plotOutput("plot1", height = 250)),
+          
+          box(
+            title = "Controls",
+            sliderInput("slider", "Number of observations:", 1, 100, 50)
+          )
+        )
       ),
       
       # Second tab content
