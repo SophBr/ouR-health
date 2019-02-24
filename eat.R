4491d7a47dccb36d1df8214577f190a20b6ae135
gabe579 <47929194+gabe579@users.noreply.github.com>
2019-02-24 09:25:26 -0500

    Update eat.R

--- eat.R
+++ eat.R
@@ -1,8 +1,11 @@
 plot.eat <- function(column, unitLabel) {
   
-  veg1<-column*.01
+  NewFrut<-filter(data,column<1000)
+  veg1<-NewFrut$column*.01
   ggplot(data.frame(veg1)) +
     geom_histogram(aes(x = veg1), bins = 5) + 
-     labs(x = paste(unitLabel, "units consumed"), y = "Number of People / Responses") +
-    scale_y_log10()
+    labs(x = paste( "units consumed"), y = "Number of People / Responses")
 }
+
+
+  
