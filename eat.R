5d8a9115403bb6de879f2fac867c373d92585cdf
gabe579 <47929194+gabe579@users.noreply.github.com>
2019-02-24 09:28:56 -0500

    Update eat.R

--- eat.R
+++ eat.R
@@ -4,7 +4,7 @@ plot.eat <- function(column, unitLabel) {
   veg1<-NewFrut$column*.01
   ggplot(data.frame(veg1)) +
     geom_histogram(aes(x = veg1), bins = 5) + 
-    labs(x = paste( "units consumed"), y = "Number of People / Responses")
+     labs(x = paste(unitLabel, "units consumed"), y = "Number of People / Responses") +
 }
 
 
