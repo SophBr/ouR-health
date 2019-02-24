63a2aab89a52223afbc89d58151fb940791ea6fc
Jan Simson <jan.simson@gmx.de>
2019-02-24 00:06:44 -0500

    Improved plot description for sleep log

--- sleep.R
+++ sleep.R
@@ -2,6 +2,6 @@ plot.sleep <- function(data) {
   sleep <- data %>% filter(SLEPTIM1 <= 24)
   ggplot(sleep) +
     geom_histogram(aes(x = SLEPTIM1), bins = 24) +
-    labs(x = "Number of Hours slept", y = "Number of Answers (log)") +
+    labs(x = "Number of Hours slept", y = "Number of People / Responses (log)") +
     scale_y_log10()
 }
