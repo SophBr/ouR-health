5ce7a132c0e8dcf28eee5de5302351445b437db8
Jan Simson <jan.simson@gmx.de>
2019-02-24 02:26:50 -0500

    fix string concatenation

--- eat.R
+++ eat.R
@@ -3,6 +3,6 @@ plot.eat <- function(column, unitLabel) {
   veg1<-column*.01
   ggplot(data.frame(veg1)) +
     geom_histogram(aes(x = veg1), bins = 5) + 
-     labs(x = unitLabel+ " units consumed", y = "Number of People / Responses") +
+     labs(x = paste(unitLabel, "units consumed"), y = "Number of People / Responses") +
     scale_y_log10()
 }
